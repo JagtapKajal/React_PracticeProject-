66a29da8db6a86511391f25071d5b4420270bd92
jagtapK <jagtapkajal54@gmail.com>
2025-08-22 23:15:07 +0530

    fixed Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import './Navbar.css';
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <div className="navbar">
-      <div className="left"></div>
+    <nav className="navbar">
+      <div className="left">
+        <h2>My App</h2>
+      </div>
       <div className="right">
+        <Link to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/contact">Contact</Link>
-        <Link to="/courses">Courses</Link> 
-        <Link to="/home">Home</Link>
+        <Link to="/courses">Courses</Link>
       </div>
-    </div>
+    </nav>
   );
 };
 
